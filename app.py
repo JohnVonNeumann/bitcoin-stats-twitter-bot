bbbe0457b69dcde4502e0a394fb2fc816e0a96c3
JohnVonNeumann <louiswillcock@gmail.com>
2017-10-12 09:00:53 +1100

    updated requirements file with peter todds python-bitcoilib, and imported and called Proxy class, it complains about lack of a conf file but that's intended as this rig doesnt run a node, will have to continue development of this from my home rig

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 
 import tweepy
+import bitcoin.rpc
 
 CONSUMER_KEY = os.getenv('CONSUMER_KEY')
 CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+
+client = bitcoin.rpc.Proxy()
