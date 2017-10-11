0bde8288dc489b81c6c7cab5276e43f95e12e253
JohnVonNeumann <louiswillcock@gmail.com>
2017-10-11 19:02:07 +1100

    updated gitignore with the usual suspects and moved api keys into .env file for effective secret management

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+
+import tweepy
+
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
