a79a85d2430dcf871b02fd001adafe7138e187fc
JohnVonNeumann <louiswillcock@gmail.com>
2017-10-12 09:57:40 +1100

    authentication still not working properly here, but another step forward, the next step is to refactor the twitter_auth and badly named total_twitter_auth vars

--- app.py
+++ app.py
@@ -11,4 +11,7 @@ ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 #client = bitcoin.rpc.Proxy()
 
 twitter_auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-twitter_auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+total_twitter_auth = twitter_auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+twitter = tweepy.API(total_twitter_auth)
+twitter.update_status('test run')
