75cee475c696e0d81ad16c03f11af93b901f0927
JohnVonNeumann <louiswillcock@gmail.com>
2017-10-12 09:13:46 +1100

    add twitter auth variables, about to get off train so need to commit now or ill lose my spot, my next step is to use the twitter_auth to create a simple test tweet from a string

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 
-client = bitcoin.rpc.Proxy()
+#client = bitcoin.rpc.Proxy()
+
+twitter_auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+twitter_auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
