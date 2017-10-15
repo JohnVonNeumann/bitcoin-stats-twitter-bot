8e2be728b0f69f5cd3c7a256897da62650243085
JohnVonNeumann <louiswillcock@gmail.com>
2017-10-15 19:24:35 +1100

    got it working with the rpc proxy, small changes just testing things to call and then layout, need to work out if i will just run as a dirty proc script or if it should use functions and whatnot

--- app.py
+++ app.py
@@ -8,10 +8,20 @@ CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 
-#client = bitcoin.rpc.Proxy()
+client = bitcoin.rpc.Proxy()
 
-twitter_auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-twitter_auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+blocks = client.call('getblockcount')
+print('#' + str(blocks))
 
-twitter = tweepy.API(twitter_auth)
-twitter.update_status('And I was given life.')
+mempool = client.call('getmempoolinfo')
+print(str(mempool['size']) + ' transactions awaiting confirmation')
+
+fee = client.call('estimatesmartfee', 6)
+print(fee)
+
+
+#twitter_auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+#twitter_auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+#
+#twitter = tweepy.API(twitter_auth)
+#twitter.update_status('And I was given life.')
